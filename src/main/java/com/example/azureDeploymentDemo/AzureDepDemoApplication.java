69c0f15f3bba5d13486c497d51a801fc7e50a255
vn51dja <lalli.priyadharshini@samsclub.com>
2023-03-23 10:59:52 +0530

    first commit

---
+++ src/main/java/com/example/azureDeploymentDemo/AzureDepDemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.azureDeploymentDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureDepDemoApplication {
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDepDemoApplication.class, args);
+	}
+
+}
